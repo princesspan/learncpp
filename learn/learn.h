342f5f573e7d394c904a797330c27a90bca22e53
Princess Pan <princesspan@protonmail.com>
2019-01-08 14:28:02 +0000

    init

---
+++ learn/learn.h
@@ -0,0 +1,6 @@
+#ifndef LEARN_H
+#define LEARN_H
+
+int add(int x, int y); // function prototype for add.h -- don't forget the semicolon!
+ 
+#endif
---
+++ learn/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "learn.h"
+
+int main(int argc, char *argv[]){
+   std::cout << "Hello World!" << add(3, 4) << std::endl;
+   return 0;
+}
+
